e596946f63d27ee44fdbc04dd79409f7d8e043f0
0xSileo <github@sileo.dev>
2025-07-22 19:20:34 +0200

    Minor: Fixed indentation

--- src/routes/address/[ethAddress]/+page.svelte
+++ src/routes/address/[ethAddress]/+page.svelte
@@ -14,19 +14,20 @@
 
 
 <table>
-    <thead>
-      <tr>
-        <th>Key</th>
-        <th>Value</th>
-      </tr>
-    </thead>
-    <tbody>
-        <ul>
-        {#each Object.entries(data) as [key, value] (key)}
+  <thead>
+    <tr>
+      <th>Key</th>
+      <th>Value</th>
+    </tr>
+  </thead>
+  <tbody>
+    <ul>
+      {#each Object.entries(data) as [key, value] (key)}
         <tr>
           <td>{camelToHuman(key)}</td>
           <td>{value}</td>
         </tr>
       {/each}
-    </tbody>
-  </table>
+    </ul>
+  </tbody>
+</table>
