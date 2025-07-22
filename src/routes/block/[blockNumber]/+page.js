559e469c34638acec9896bae67e9bac653650772
0xSileo <github@sileo.dev>
2025-07-22 19:24:29 +0200

    Minor : Added comments and error messages

--- src/routes/block/[blockNumber]/+page.js
+++ src/routes/block/[blockNumber]/+page.js
@@ -2,26 +2,35 @@ export async function load({ params }) {
 
   let block;
   let blockNumber;
+  let requestedLatestBlock = false;
 
+  // Blocks can be fetched by specifying
+
+  // 1. "latest" : Handle case where url is /block/latest
   if (params.blockNumber == "latest") {
-    params.blockNumber = await getBlockNumber()
+    params.blockNumber = await getBlockNumber();
+    requestedLatestBlock = true;
   }
 
-  console.log(params.blockNumber)
+  // 2. The block number : Check & handle case of uint
   if (isDecimalUint(params.blockNumber)) {
 
     blockNumber = parseInt(params.blockNumber)
     block = await getBlockByNumber(blockNumber)
 
+  // 3. The block hash : Check & handle case of 32 hex bytes (blockhash)
   } else if (isBytes32Hex(params.blockNumber)) {
-
+    
+    // TODO Error handling if it's an inexistent hash
     let blockHash = params.blockNumber.startsWith("0x") ? params.blockNumber : "0x" + params.blockNumber;
+
     block = await getBlockByHash(blockHash)
     blockNumber = parseInt(block.number)
 
   } else {
-    throw new Error('Block number not decimal or not hash type');
+    throw new Error("Invalid block reference given. User has a 500 page now");
   }
+
   const currentBlockNumber = await getBlockNumber()
 
     // Return the data to be used in the component
