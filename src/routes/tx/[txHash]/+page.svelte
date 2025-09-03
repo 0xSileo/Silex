56e9b91bf0e10a820b8a3c87dbd840be49d99ed3
0xSileo <github@sileo.dev>
2025-09-03 15:02:13 +0200

    Added display of units for txs

--- src/routes/tx/[txHash]/+page.svelte
+++ src/routes/tx/[txHash]/+page.svelte
@@ -2,7 +2,9 @@
   import { camelToHuman } from '$lib/utils/utils';
   
   export let data;
-  const numberKeys = ["nonce","transactionIndex","value","gasPrice","gas","maxFeePerGas","maxPriorityFeePerGas","type"];
+  const numberKeys = ["nonce","transactionIndex","gas","type", "chainId"];
+  const ethKeys = ["value"];
+  const gweiKeys = ["gasPrice","maxFeePerGas","maxPriorityFeePerGas",];
 
 
 </script>
@@ -23,6 +25,10 @@ Transaction details
           <td>{camelToHuman(key)}</td>
           {#if numberKeys.includes(key)}
           <td>{parseInt(value)}</td>
+          {:else if ethKeys.includes(key)}
+          <td>{parseInt(value)/1e18} ETH</td>
+          {:else if gweiKeys.includes(key)}
+          <td>{parseInt(value)/1e9} gwei</td>
           {:else if key=="blockHash"}
           <td><code><a href="/block/{value}">{value}</a></code></td>
           {:else if key=="blockNumber"}
