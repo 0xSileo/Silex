8f65e0d01b6ab5fe33fb2123459799fcdd89bc36
0xSileo <github@sileo.dev>
2025-09-17 13:28:06 +0200

    Added ENS primary name resolution to address page

--- src/routes/address/[ethAddress]/+page.svelte
+++ src/routes/address/[ethAddress]/+page.svelte
@@ -15,10 +15,17 @@
     <ul>
       {#each Object.entries(data) as [key, value] (key)}
         <tr>
-          <td>{camelToHuman(key)}</td>
+          <td>{key == "primaryENS" ? "Primary ENS" : camelToHuman(key)}</td>
           <td>{value}</td>
         </tr>
       {/each}
     </ul>
   </tbody>
 </table>
+
+
+<h1>Address Info</h1>
+<p><strong>Address:</strong> {data.primaryENS || data.ethAddress}</p>
+<p><strong>Balance:</strong> {data.balance}</p>
+<p><strong>Transaction Count:</strong> {data.txCount}</p>
+<p><strong>Code:</strong> {data.code === '0x' ? 'Externally Owned Account' : 'Smart Contract'}</p>
