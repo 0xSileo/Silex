ce8d730635e3dc4e8508a2b0885cc858d3f17f7b
0xSileo <github@sileo.dev>
2025-09-17 14:16:55 +0200

    Added promise concurrency in address server page

--- src/routes/address/[ethAddress]/+page.server.js
+++ src/routes/address/[ethAddress]/+page.server.js
@@ -1,18 +1,20 @@
 import { getTransactionCount, getBalance, getCode, resolveENSFromAddress } from "/src/lib/utils/utils"
 
 export async function load({ params }) {
-    const ethAddress = params.ethAddress
-    const balance = await getBalance(ethAddress)
-    const txCount = await getTransactionCount(ethAddress)
-    const code = await getCode(ethAddress)
-    const primaryENS = await resolveENSFromAddress(ethAddress)
+  const ethAddress = params.ethAddress;
 
-    // Return the data to be used in the component
-    return {
-      ethAddress,
-      balance,
-      txCount,
-      code,
-      primaryENS
-    };
+  const [balance, txCount, code, primaryENS] = await Promise.all([
+    getBalance(ethAddress),
+    getTransactionCount(ethAddress),
+    getCode(ethAddress),
+    resolveENSFromAddress(ethAddress)
+  ]);
+
+  return {
+    ethAddress,
+    balance,
+    txCount,
+    code,
+    primaryENS
+  };
 }
